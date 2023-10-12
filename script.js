7d56faf0fa5bdd3fc9028977339fa88f43d93db5
gltats <tatianasofiagomes831@gmail.com>
2023-10-12 18:33:27 +0200

    ready

--- script.js
+++ script.js
@@ -9,3 +9,18 @@ for (let i = 0; i < numRows; i++) {
     row.style.animation = `moveSideways ${10 + i * 0.5}s linear infinite alternate`; // Adjust the animation duration
     movingText.appendChild(row);
 }
+
+// Load visitor count from localStorage or initialize it to 0
+let visitorCount = localStorage.getItem('visitorCount');
+if (visitorCount === null) {
+    visitorCount = 0;
+}
+visitorCount = parseInt(visitorCount, 10);
+
+// Increment the visitor count for each page load
+visitorCount++;
+localStorage.setItem('visitorCount', visitorCount);
+
+// Update the counter element
+const counterElement = document.getElementById('counter');
+counterElement.innerText = `Unique Visitors: ${visitorCount}`;
